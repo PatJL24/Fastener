5bd47129079b12137767e302d997be9660b8d3cd
Patrick Li <li.patrick107@gmail.com>
2020-03-21 19:09:53 -0400

    Added toString Method to InnerThreaded class and added comments to both classes.

--- src/InnerThreaded.java
+++ src/InnerThreaded.java
@@ -2,9 +2,13 @@ import java.io.Serializable;
 
 public class InnerThreaded extends Threaded implements Serializable {
 
+    //InnerThreaded Constructor
     public InnerThreaded(Threads threadSize, Enum<?> material, Enum<?> finish, double unitPrice, int numPerUnit) throws IllegalFastener {
         super(threadSize, material, finish, unitPrice, numPerUnit);
     }
 
-
+    //Returns a String
+    public String toString(){
+        return super.toString();
+    }
 }
--- src/OuterThreaded.java
+++ src/OuterThreaded.java
@@ -5,11 +5,13 @@ public class OuterThreaded extends Threaded implements Serializable {
     private double length;
     private Threads threadSize;
 
+    //OuterThreaded Constructor
     public OuterThreaded(double length, Threads threadSize, Enum<?> material, Enum<?> finish, double unitPrice, int numPerUnit) throws IllegalFastener {
         super(threadSize, material, finish, unitPrice, numPerUnit);
         setLength(length);
     }
 
+    //Sets the length if the length is valid.
     public void setLength(double len) throws IllegalFastener {
         if (len >= 0.5 && len <= 6.0 && len % 0.25 == 0) this.length = len;
         else if (len >= 6 && len <= 11 && len % 0.5 == 0) this.length = len;
@@ -17,6 +19,7 @@ public class OuterThreaded extends Threaded implements Serializable {
         else throw new IllegalFastener("Illegal length: " + len);
     }
 
+    //Returns a String.
     public String toString(){
         return this.length + "\" long, " + super.toString();
     }
